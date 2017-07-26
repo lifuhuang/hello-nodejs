a94c608a765a24a9bc2115d00af89ebbdb481c6f
Lifu Huang <lifhuan@microsoft.com>
2017-07-26 15:41:10 -0700

    Add patch put delete method for book.

--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -20,15 +20,57 @@ const bookRouter = (Book) => {
             res.status(201).send(book);
         });
 
+    bookRouter.use('/:bookId', (req, res, next) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if (err) {
+                res.status(500).send(err);
+            } else if (book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('No book found.');
+            }
+        })
+    });
+
     bookRouter.route('/:bookId')
         .get((req, res) => {
-            Book.findById(req.params.bookId, (err, book) => {
+            res.json(req.book);
+        })
+        .put((req, res) => {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.save(err => {
                 if (err) {
                     res.status(500).send(err);
                 } else {
-                    res.json(book)
+                    res.json(req.book);
                 }
-            })
+            });
+        })
+        .patch((req, res) => {
+            for (const key in req.body) {
+                if (req.body.hasOwnProperty(key) && key !== '_id') {
+                    req.book[key] = req.body[key];
+                }
+            }
+
+            req.book.save(err => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .delete((req, res) => {
+            req.book.remove(err => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('Removed');
+                }
+            });
         });
 
     return bookRouter;
